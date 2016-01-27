a5028f88931b682fe7ccda25edfa116327ba3fe0
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-26 21:47:02 -0500

    Array

---
+++ Arrays/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAddArray = New System.Windows.Forms.Button()
+        Me.listArrays = New System.Windows.Forms.ListBox()
+        Me.btnSortArray = New System.Windows.Forms.Button()
+        Me.btnReverse = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAddArray
+        '
+        Me.btnAddArray.Location = New System.Drawing.Point(254, 26)
+        Me.btnAddArray.Name = "btnAddArray"
+        Me.btnAddArray.Size = New System.Drawing.Size(126, 23)
+        Me.btnAddArray.TabIndex = 0
+        Me.btnAddArray.Text = "Add Array Elements"
+        Me.btnAddArray.UseVisualStyleBackColor = True
+        '
+        'listArrays
+        '
+        Me.listArrays.FormattingEnabled = True
+        Me.listArrays.Location = New System.Drawing.Point(12, 26)
+        Me.listArrays.Name = "listArrays"
+        Me.listArrays.Size = New System.Drawing.Size(222, 186)
+        Me.listArrays.TabIndex = 1
+        '
+        'btnSortArray
+        '
+        Me.btnSortArray.Location = New System.Drawing.Point(254, 83)
+        Me.btnSortArray.Name = "btnSortArray"
+        Me.btnSortArray.Size = New System.Drawing.Size(126, 23)
+        Me.btnSortArray.TabIndex = 2
+        Me.btnSortArray.Text = "Sort Array"
+        Me.btnSortArray.UseVisualStyleBackColor = True
+        '
+        'btnReverse
+        '
+        Me.btnReverse.Location = New System.Drawing.Point(254, 127)
+        Me.btnReverse.Name = "btnReverse"
+        Me.btnReverse.Size = New System.Drawing.Size(126, 23)
+        Me.btnReverse.TabIndex = 3
+        Me.btnReverse.Text = "Reverse Array"
+        Me.btnReverse.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(438, 261)
+        Me.Controls.Add(Me.btnReverse)
+        Me.Controls.Add(Me.btnSortArray)
+        Me.Controls.Add(Me.listArrays)
+        Me.Controls.Add(Me.btnAddArray)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnAddArray As Button
+    Friend WithEvents listArrays As ListBox
+    Friend WithEvents btnSortArray As Button
+    Friend WithEvents btnReverse As Button
+End Class
---
+++ Arrays/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private strClasses() As String = {"Web Design", "Spanish", "Gym", "English", "History", "Calculus"}
+    Private myNumbers() As Integer = {1, 2, 3, 4, 5, 6}
+
+    Private Sub btnAddArray_Click(sender As Object, e As EventArgs) Handles btnAddArray.Click
+        ClearList()
+
+        AddItemsToList(myNumbers)
+
+
+
+    End Sub
+
+    Private Sub ClearList()
+        listArrays.Items.Clear()
+    End Sub
+
+    Private Sub AddItemsToList(ByVal argArray() As String)
+        'Enumerate Array
+        For Each item As String In argArray
+            'add array item to list
+            listArrays.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub addItemsToList(ByVal argArray() As Integer
+            'Enumerate Array
+        For Each item As String In argArray
+            'add array item to list
+            listArrays.Items.Add(item)
+        Next
+
+
+
+
+
+    Private Sub btnSortArray_Click(sender As Object, e As EventArgs) Handles btnSortArray.Click
+        ClearList()
+
+        Array.Sort(strClasses)
+        AddItemsToList(strClasses)
+
+
+    End Sub
+
+
+
+    Private Sub btnReverse_Click(sender As Object, e As EventArgs) Handles btnReverse.Click
+        ClearList()
+
+        Array.Reverse(strClasses)
+        AddItemsToList(strClasses)
+        Array.Sort(strClasses)
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        strClasses(0) = "Web Design"
+        strClasses(1) = "Spanish"
+        strClasses(2) = "Gym"
+        strClasses(3) = "English"
+        strClasses(4) = "History"
+        strClasses(5) = "Calculus"
+
+    End Sub
+End Class
